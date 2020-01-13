a31de7afcb2b968eca0dbf833088210bd7e5cf47
chetting <chetting@stud.hs-offenburg.de>
2020-01-13 21:13:45 +0100

    init + start page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { StartComponent } from './components/start/start.component'
+import { NavigationComponent } from './components/navigation/navigation.component';
+
+const routes: Routes = [
+  { path: 'start', component: StartComponent },
+  { path: 'navigation', component: NavigationComponent },
+  { path: '', redirectTo: 'start', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {SuiModule} from 'ng2-semantic-ui';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { StartComponent } from './components/start/start.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    SuiModule,
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
